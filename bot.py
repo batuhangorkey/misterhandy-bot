3c08fb7f82709a847837f1ea83bc3dd22244b750
Batuhan <41853198+batuhan@users.noreply.github.com>
2020-06-07 04:28:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,4 +106,8 @@ async def func3(ctx):
     await ctx.send(f'Deleted {len(deleted)} message(s).')
 
 
+@bot.command(name='del_all', help='Tries to purge messages. (limit 50)')
+async def func4(ctx):
+    deleted = await ctx.channel.purge(limit=50)
+    await ctx.send(f'Deleted {len(deleted)} message(s).')
 bot.run(TOKEN)
