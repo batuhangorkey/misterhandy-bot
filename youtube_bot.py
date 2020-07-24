4b5f8333455bce533980d8c4fc7cd9a85efc9d5f
Batuhan <41853198+batuhan@users.noreply.github.com>
2020-07-24 13:52:58 +0300

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -27,11 +27,10 @@ ffmpeg_options = {
     'options': '-vn'
 }
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 # Çalışmıyor
 # if not discord.opus.is_loaded():
 #     discord.opus.load_opus('opus')
-
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 default_presence = discord.Activity(type=discord.ActivityType.listening, name='wasteland with sensors offline')
 
 
@@ -69,19 +68,20 @@ class Music(commands.Cog):
             current = await self.queue.get()
             ctx = current[0]
             player = current[1]
-            ctx.voice_client.play(player, after=lambda e: self.toggle_next(e, loop=self.bot.loop))
+            ctx.voice_client.play(player, after=lambda e: self.after_voice(e, ctx, loop=self.bot.loop))
             await ctx.send('Now playing: {}'.format(player.title))
             await self.play_next.wait()
 
-    # async def after_voice(self, ctx):
-    #     await self.bot.wait_until_ready()
-    #     while ctx.voice_client.is_playing():
-    #         await asyncio.sleep(1)
-    #     await ctx.send('Finished playing.')
-    def toggle_next(self, e: Exception, loop=None):
+    async def after_voice(self, e: Exception, ctx, loop=None):
         if e is not None:
             print('Player error: %s' % e)
-        # loop.create_task(self.after_voice(ctx))
+        await self.bot.wait_until_ready()
+        while ctx.voice_client.is_playing():
+            await asyncio.sleep(1)
+        await ctx.send('Finished playing.')
+        self.toggle_next(loop)
+
+    def toggle_next(self, loop=None):
         loop.call_soon_threadsafe(self.play_next.set)
 
     @commands.command(help='Joins authors voice channel.')
@@ -91,13 +91,13 @@ class Music(commands.Cog):
 
         await channel.connect()
 
+    # sıraya eklemiyor mevcut çalan videonun yerine geçiyor
     @commands.command(help="Plays from a url.")
     async def yt(self, ctx, *, url):
         loop = self.bot.loop
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=loop)
-            # 'after=lambda e: print('Player error: %s' % e) if e else None' çıkartıldı.
-            ctx.voice_client.play(player, after=lambda e: self.toggle_next(e, loop=loop))
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
         await ctx.send('Now playing: {}'.format(player.title))
 
@@ -109,7 +109,6 @@ class Music(commands.Cog):
             # ctx.voice_client.play(player, after=lambda e: self.toggle_next(ctx, e, loop=loop))
         # sıraya ekle
         await self.queue.put((ctx, player))
-        await ctx.send('Now playing: {}'.format(player.title))
         # Durumu değiştir
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,
                                                                  name='{}'.format(player.title)))
