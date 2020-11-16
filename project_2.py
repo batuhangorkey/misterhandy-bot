afe8058e035be4e00dc7a08ce60bcf9a03d0ea6b
Batuhan <41853198+batuhan@users.noreply.github.com>
2020-11-17 02:24:05 +0300

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -2,33 +2,35 @@ import random
 from discord.ext import commands
 
 
-class Project2(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.storyteller = storyteller
-
-    @commands.command(help='Generate random text')
-    async def bar(self, ctx, input_text=None):
-        if input_text is None:
-            await ctx.send(self.storyteller.view_room())
-        else:
-            self.storyteller.progress(input_text)
-            await ctx.send(self.storyteller.view_room())
-
-    @commands.command(help='Start anew')
-    async def reset(self, ctx):
-        self.storyteller = StoryTeller(room1)
+# class Project2(commands.Cog):
+#     def __init__(self, bot):
+#         self.bot = bot
+#         self.storyteller = storyteller
+#
+#     @commands.command(help='Generate random text')
+#     async def bar(self, ctx, input_text=None):
+#         if input_text is None:
+#             await ctx.send(self.storyteller.view_room())
+#         else:
+#             self.storyteller.progress(input_text.lower())
+#             await ctx.send(self.storyteller.view_room())
+#
+#     @commands.command(help='Start anew')
+#     async def reset(self, ctx):
+#         self.storyteller = StoryTeller(rooms[0])
 
 
 class StoryTeller:
     def __init__(self, starting_room):
         self.current_room = starting_room
+        self.gender = 0
 
     def progress(self, input_text):
         for _ in self.current_room.exits:
-            if input_text == _.key:
-                self.current_room = _.room
-                return True
+            for key in _.keys:
+                if input_text == key:
+                    self.current_room = _.room
+                    return True
         return False
 
     def view_room(self):
@@ -36,21 +38,34 @@ class StoryTeller:
 
 
 class Exit:
-    def __init__(self, key, room):
-        self.key = key
+    def __init__(self, keys, room=None, action=None):
+        self.keys = keys
         self.room = room
+        self.action = action
 
 
 class Room:
-    def __init__(self, description, exits=None):
+    def __init__(self, description, *exits):
         self.description = description
         self.exits = exits
 
 
-room1 = Room("Barda güzel bir hanımefendiyle karşılaştın. Eve gidiyor musun?")
-room2 = Room("Evdesiniz.")
+rooms = [
+    Room("Barda beğendiğin güzel bir hanımefendi ve bir beyefendi var. Hangisine yaklaşıyorsun?"),
+    Room("Hanımefendi evine davet etti. Evine gidiyor musun?"),
+    Room("Evdesiniz. Trans olduğunu öğreniyorsun. Devam ediyor musun?"),
+    Room("Ağzına almanı istiyor. Devam ediyor musun?"),
+    Room("Sana arkadan girmek istiyor. Devam ediyor musun?"),
+    Room("Ağzına boşalmak istiyor. Devam ediyor musun?"),
+    Room("Davet eder gibi arkasını dönüp yatağa uzanıyor. Devam ediyor musun?"),
+    Room("Güzel bir geceydi.")
+]
 
-room1.exits += Exit("evet", room2)
+rooms[0].exits = Exit(["kadın", "erkek"], rooms[1], True)
+rooms[1].exits = Exit("evet", rooms[2])
 
+storyteller = StoryTeller(rooms[0])
 
-storyteller = StoryTeller(room1)
+print(storyteller.view_room())
+storyteller.progress("kadın")
+print(storyteller.view_room())
