77d08050399e9a6ff733d0986d606843829b8117
Batuhan <41853198+batuhan@users.noreply.github.com>
2020-11-21 22:33:49 +0300

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -43,7 +43,7 @@ class StoryTeller:
     def view_room(self):
         if self.current_room.switch is not None:
             for _ in self.current_room.trigger:
-                if self.partner["gender"] == _:
+                if self.partner[self.current_room.switch] == _:
                     index = self.current_room.trigger.index(_)
                     return self.current_room.description[index].format(**self.partner)
         return self.current_room.description.format(**self.partner)
@@ -71,33 +71,41 @@ def change_gender(self: StoryTeller, text_input):
         self.partner["gender"] = 1
 
 
-rooms = [
-    Room("Barda beğendiğin güzel bir hanımefendi ve bir beyefendi var. Hangisine yaklaşıyorsun?"),
-    Room(["Birbirinizle mükemmel uyuşuyorsunuz. Beyefendi evine davet ediyor. Evine gidiyor musun?",
-          "Birbirinizle mükemmel uyuşuyorsunuz. Hanımefendi evine davet ediyor. Evine gidiyor musun?"],
-         [0, 1],
-         "gender"),
-    Room(["Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Devam ediyor musun?",
-          "Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Penisi {penis_size} santim."
-          " Devam ediyor musun?"],
-         [0, 1],
-         "gender"),
-    Room("Ağzına almanı istiyor. Devam ediyor musun?"),
-    Room("Sana arkadan girmek istiyor. Devam ediyor musun?"),
-    Room("Ağzına boşalmak istiyor. Devam ediyor musun?"),
-    Room("Ona arkadan girmeni istiyor. Devam ediyor musun?"),
-    Room("Güzel bir geceydi."),
-    Room("Evine yalnız dönüyorsun.")
-]
+rooms = {
+    1: Room("Barda beğendiğin güzel bir hanımefendi ve bir beyefendi var. Hangisine yaklaşıyorsun?"),
+    2: Room(["Birbirinizle mükemmel uyuşuyorsunuz. Beyefendi evine davet ediyor. Evine gidiyor musun?",
+             "Birbirinizle mükemmel uyuşuyorsunuz. Hanımefendi evine davet ediyor. Evine gidiyor musun?"],
+            [0, 1],
+            "gender"),
+    3: Room(["Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Devam ediyor musun?",
+             "Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Penisi {penis_size} santim."
+             " Ne yapıyorsun?"],
+            [0, 1],
+            "gender"),
+    4: Room("Ağzına almanı istiyor. Devam ediyor musun?"),
+    5: Room(["Sana arkadan straponla girmek istiyor. Devam ediyor musun?",
+             "{penis_size} santimlik penisiyle sana arkadan girmek istiyor. Alıyor musun?"],
+            [0, 1],
+            "gender"),
+    6: Room("Ağzına boşalmak istiyor. Devam ediyor musun?"),
+    7: Room("Ona arkadan girmeni istiyor. Devam ediyor musun?"),
+    8: Room("Güzel bir geceydi."),
+    9: Room("Evine yalnız dönüyorsun."),
+    10: Room("Kolundan tutup penisini saklayabileceğini, böyle davranmamanı söylüyor."
+             " Hala gitmek istediğinden emin misin?"),
+    11: Room("Evden hızlıca çıkıyorsun.")
+}
 
-rooms[0].exits.append(Exit(["kadın", "erkek"], rooms[1], change_gender))
-rooms[1].exits.append(Exit(["evet"], rooms[2]))
+rooms[1].exits.append(Exit(["kadın", "erkek"], rooms[2], change_gender))
 rooms[2].exits.append(Exit(["evet"], rooms[3]))
-rooms[3].exits.append(Exit(["evet"], rooms[4]))
+rooms[3].exits.append(Exit(["evet", "sevişiyorum", "sikiyorum", "seks yapıyorum"], rooms[3]))
+rooms[3].exits.append(Exit(["kaçıyorum"], rooms[10]))
+rooms[10].exits.append(Exit(["evet", "eminim"], rooms[11]))
 rooms[4].exits.append(Exit(["evet"], rooms[5]))
 rooms[5].exits.append(Exit(["evet"], rooms[6]))
 rooms[6].exits.append(Exit(["evet"], rooms[7]))
-rooms[1].exits.append(Exit(["hayır"], rooms[8]))
+rooms[7].exits.append(Exit(["evet"], rooms[8]))
+rooms[8].exits.append(Exit(["hayır"], rooms[9]))
 
 # storyteller = StoryTeller(rooms[0])
 #
