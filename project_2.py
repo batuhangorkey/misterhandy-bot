7c1bae296ca9416ed37b522c8768bc9ed08a2e12
Batuhan <41853198+batuhan@users.noreply.github.com>
2020-11-21 21:44:25 +0300

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -23,7 +23,10 @@ class Project2(commands.Cog):
 class StoryTeller:
     def __init__(self, starting_room):
         self.current_room = starting_room
-        self.gender = 0
+        self.partner = {
+            "penis_size": random.randint(10, 25),
+            "gender": 0,
+        }
 
     def progress(self, input_text):
         for _ in self.current_room.exits:
@@ -36,10 +39,12 @@ class StoryTeller:
         return False
 
     def view_room(self):
-        if rooms.index(self.current_room) == 1:
-            return self.current_room.description.format("Hanımefendi" if self.gender else "Beyefendi")
-        penis_desc = " Penisi {} santim.".format(random.randint(10, 25)) if self.gender else ""
-        return self.current_room.description.format(penis_desc=penis_desc)
+        if self.current_room.switch is not None:
+            for _ in self.current_room.trigger:
+                if self.partner["gender"] == _:
+                    index = self.current_room.trigger.index(_)
+                    return self.current_room.description[index].format(**self.partner)
+        return self.current_room.description.format(**self.partner)
 
 
 class Exit:
@@ -50,23 +55,31 @@ class Exit:
 
 
 class Room:
-    def __init__(self, description, _format=None):
+    def __init__(self, description, trigger=None, switch=None):
         self.description = description
         self.exits = []
-        self.format = _format
+        self.trigger = trigger
+        self.switch = switch
 
 
-def change_gender(self, text_input):
+def change_gender(self: StoryTeller, text_input):
     if text_input == "erkek":
-        self.gender = 0
+        self.partner["gender"] = 0
     else:
-        self.gender = 1
+        self.partner["gender"] = 1
 
 
 rooms = [
     Room("Barda beğendiğin güzel bir hanımefendi ve bir beyefendi var. Hangisine yaklaşıyorsun?"),
-    Room("Birbirinizle mükemmel uyuşuyorsunuz. {} evine davet ediyor. Evine gidiyor musun?"),
-    Room("Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun.{penis_desc} Devam ediyor musun?"),
+    Room(["Birbirinizle mükemmel uyuşuyorsunuz. Beyefendi evine davet ediyor. Evine gidiyor musun?",
+          "Birbirinizle mükemmel uyuşuyorsunuz. Hanımefendi evine davet ediyor. Evine gidiyor musun"],
+         [0, 1],
+         "gender"),
+    Room(["Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Devam ediyor musun?",
+          "Yatak odasına kadar ilerlediniz. Trans olduğunu öğreniyorsun. Penisi {penis_size} santim."
+          " Devam ediyor musun?"],
+         [0, 1],
+         "gender"),
     Room("Ağzına almanı istiyor. Devam ediyor musun?"),
     Room("Sana arkadan girmek istiyor. Devam ediyor musun?"),
     Room("Ağzına boşalmak istiyor. Devam ediyor musun?"),
@@ -75,15 +88,15 @@ rooms = [
     Room("Evine yalnız dönüyorsun.")
 ]
 
-rooms[0].exits.append(Exit(["kadın", "erkek"], rooms[1], change_gender))
-rooms[1].exits.append(Exit(["evet"], rooms[2]))
-rooms[2].exits.append(Exit(["evet"], rooms[3]))
-rooms[3].exits.append(Exit(["evet"], rooms[4]))
-rooms[4].exits.append(Exit(["evet"], rooms[5]))
-rooms[5].exits.append(Exit(["evet"], rooms[6]))
-rooms[6].exits.append(Exit(["evet"], rooms[7]))
-rooms[1].exits.append(Exit(["hayır"], rooms[8]))
-
+# rooms[0].exits.append(Exit(["kadın", "erkek"], rooms[1], change_gender))
+# rooms[1].exits.append(Exit(["evet"], rooms[2]))
+# rooms[2].exits.append(Exit(["evet"], rooms[3]))
+# rooms[3].exits.append(Exit(["evet"], rooms[4]))
+# rooms[4].exits.append(Exit(["evet"], rooms[5]))
+# rooms[5].exits.append(Exit(["evet"], rooms[6]))
+# rooms[6].exits.append(Exit(["evet"], rooms[7]))
+# rooms[1].exits.append(Exit(["hayır"], rooms[8]))
+#
 # storyteller = StoryTeller(rooms[0])
 #
 # print(storyteller.view_room())
